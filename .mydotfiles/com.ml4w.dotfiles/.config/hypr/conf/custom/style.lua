fb78aec3da2ceac4ed4766e92dc39b9f089c8a46
yurihikari <quangminhjean@gmail.com>
2026-05-27 23:14:25 +0200

    feat: :lipstick: Added xwayland zero scaling and added swayNC layer rule for blurr
    Added xwayland zero scaling and added swayNC layer rule for blurr

--- .mydotfiles/com.ml4w.dotfiles/.config/hypr/conf/custom/style.lua
+++ .mydotfiles/com.ml4w.dotfiles/.config/hypr/conf/custom/style.lua
@@ -8,3 +8,22 @@ hl.layer_rule({
     xray = true,
     ignore_alpha = 0.79
 })
+
+-- SwayNC layer rule
+hl.layer_rule({
+    match = {
+        namespace = "swaync-control-center"
+    },
+    blur_popups = true,
+    blur = true,
+    xray = true,
+    ignore_alpha = 0.79
+})
+
+
+-- unscale XWayland
+hl.config({
+  xwayland = {
+    force_zero_scaling = true
+  }
+})
